9b91c32f14628182be4ac929168395fc026b79d6
gnomemage1990@gmail.com <gnomemage1990@gmail.com>
2024-03-15 00:07:40 +0800

    final commit

--- python/resources/inventory.py
+++
@@ -1,7 +0,0 @@
-from flask import request
-from flask_restful import Resource
-
-class Inventory(Resource):
-    def get(self):
-        query = request.args.get("query", None)
-        return {"message": "Missing required query param"}, 400
